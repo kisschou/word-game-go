ce1b4c1da2294986fe45373e8ec186a5e15b71e9
Kisschou <kisschou@me.com>
2020-05-18 11:05:39 +0000

    新增jwt脚本

---
+++ tdog/core/jwt.go
@@ -0,0 +1,41 @@
+package core
+
+import (
+	"time"
+
+	"wordgame/tdog/lib"
+)
+
+type (
+	// 用来表明签名的加密算法 token 类型等
+	JwtHeader struct {
+		Type      string // 类型
+		Algorithm string // 加密算法
+	}
+
+	// Payload 记录你需要的信息。 其中应该包含 Claims
+	JwtPayload map[string]interface{}
+
+	// 通过 header 生明的加密方法生成 签名
+	JwtSignature string
+
+	// jwt 数据
+	Jwt struct {
+		header    string
+		payload   string
+		signature string
+	}
+)
+
+func (header *JwtHeader) New() {
+}
+
+func (jwt *Jwt) New(data JwtPayload) {
+	payload := make(map[string]interface{})
+	payload["data"] = data
+	payload["ita"] = time.Now().Unix()
+	payload["exp"] = 7200
+}
+
+func (jwt *Jwt) Get(data string) {
+}
