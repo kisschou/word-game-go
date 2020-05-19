dd95f6d337b5171eff501cd024ecd68d2b18152c
Kisschou <kisschou@me.com>
2020-05-19 10:43:59 +0000

    完成jwt功能(未测试)

--- tdog/core/jwt.go
+++ tdog/core/jwt.go
@@ -3,6 +3,8 @@ package core
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"wordgame/tdog/lib"
@@ -29,20 +31,110 @@ type (
 	}
 )
 
-func (header *JwtHeader) New() {
+func (header *JwtHeader) New() *JwtHeader {
+	return &JwtHeader{
+		Type:      "JWT",
+		Algorithm: "HS256",
+	}
 }
 
-func (jwt *Jwt) New(data JwtPayload) {
+/**
+ * USAGE:
+ * jwt := new(core.Jwt)
+ * data := make(map[string]interface{})
+ * data["username"] = username
+ * data["password"] = password
+ * jwt.New(data)
+ **/
+func (jwt *Jwt) New(data JwtPayload) string {
+	crypt := new(lib.Crypt)
+
+	// header
+	jwtHeader := make(map[string]string)
+	header := new(JwtHeader)
+	header = header.New()
+	jwtHeader["type"] = header.Type
+	jwtHeader["alg"] = header.Algorithm
+	jsonData, _ := json.Marshal(jwtHeader)
+	crypt.Str = string(jsonData)
+	jwt.header = crypt.Base64Encode()
+
+	// payload
 	payload := make(map[string]interface{})
 	payload["data"] = data
 	payload["ita"] = time.Now().Unix()
 	payload["exp"] = 7200
-
-	crypt := new(lib.Crypt)
-	jsonData, _ := json.Marshal(payload)
+	jsonData, _ = json.Marshal(payload)
 	crypt.Str = string(jsonData)
-	fmt.Println(crypt.Base64Encode())
+	jwt.payload = crypt.Base64Encode()
+
+	// signature
+	conf := new(lib.Config)
+	jsonData, _ = json.Marshal(payload)
+	crypt.Str = string(jsonData) + conf.Get("hex_key").String()
+	jwt.signature = crypt.Sha256()
+
+	return jwt.header + "." + jwt.payload + "." + jwt.signature
 }
 
-func (jwt *Jwt) Get(data string) {
+func (jwt *Jwt) Walk(data string) *Jwt {
+	jwtData := strings.Split(data, ".")
+	return &Jwt{
+		header:    jwtData[0],
+		payload:   jwtData[1],
+		signature: jwtData[2],
+	}
+}
+
+func (jwt *Jwt) Check(data string) bool {
+	// check isExists in redis.
+	redis := new(lib.Redis)
+	redis.NewEngine()
+	if redis.Engine.Exists("jwt:"+data).Val() < 1 {
+		return false
+	}
+
+	jwt = jwt.Walk(data)
+	crypt := new(lib.Crypt)
+
+	// check header.
+	header := new(JwtHeader)
+	header = header.New()
+	crypt.Str = jwt.header
+	jwtHeader := make(map[string]string)
+	json.Unmarshal([]byte(crypt.Base64Decode()), &jwtHeader)
+	if jwtHeader["type"] != header.Type || jwtHeader["alg"] != header.Algorithm {
+		return false
+	}
+
+	// check payload.
+	crypt.Str = jwt.payload
+	jwtPayload := make(map[string]interface{})
+	json.Unmarshal([]byte(crypt.Base64Decode()), &jwtPayload)
+	ita, _ := strconv.Atoi(fmt.Sprintf("%1.0f", jwtPayload["ita"]))
+	exp, _ := strconv.Atoi(fmt.Sprintf("%1.0f", jwtPayload["exp"]))
+	ita = ita + exp
+	if ita < int(time.Now().Unix()) {
+		return false
+	}
+
+	// check signature.
+	conf := new(lib.Config)
+	crypt.Str = jwt.payload
+	crypt.Str = crypt.Base64Decode() + conf.Get("hex_key").String()
+	if jwt.signature != crypt.Sha256() {
+		return false
+	}
+
+	return true
+}
+
+func (jwt *Jwt) Get(data string, key string) interface{} {
+	jwt = jwt.Walk(data)
+	crypt := new(lib.Crypt)
+	crypt.Str = jwt.payload
+	jwtPayload := make(map[string]interface{})
+	json.Unmarshal([]byte(crypt.Base64Decode()), &jwtPayload)
+	list := jwtPayload["data"].(map[string]interface{})
+	return list[key]
 }
