9311b08348f7d5157689885bb94170bf8b34a77f
kisschou <kisschou@me.com>
2020-06-01 01:41:58 +0800

    Feign基本功能，未完成，可能涉及到Request脚本的修改

--- app/controllers/feign.go
+++ app/controllers/feign.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"encoding/json"
+
 	"wordgame/tdog/core"
 )
 
@@ -11,4 +13,8 @@ type (
 )
 
 func (feign *Feign) Http() {
+	data := feign.Base.Req.Put
+	dataJson, _ := json.Marshal(data)
+	FeignCore := core.NewFeign()
+	FeignCore.Decoder(string(dataJson)).Target()
 }
--- tdog/core/feign.go
+++ tdog/core/feign.go
@@ -1,18 +1,64 @@
 package core
 
 import (
-	_ "wordgame/tdog/lib"
+	"encoding/json"
+	"fmt"
+
+	"wordgame/tdog/lib"
 )
 
 type (
 	Feign struct {
-		Method      string
-		BaseUrlType string
-		Url         string
-		Header      string
-		Body        string
+		Method    string
+		BaseUrl   string
+		ActionUrl string
+		Header    map[string]string
+		Body      map[string]interface{}
 	}
 )
 
-func (feign *Feign) NewClient() {
+func NewFeign() *Feign {
+	feign := &Feign{}
+	return feign
+}
+
+func (feign *Feign) Decoder(data string) *Feign {
+	dataMap := make(map[string]interface{})
+	err := json.Unmarshal([]byte(data), &dataMap)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	feign.Method = dataMap["method"].(string)
+	feign.BaseUrl = dataMap["base_url"].(string)
+	feign.ActionUrl = dataMap["action_url"].(string)
+	headerMap := make(map[string]string)
+	json.Unmarshal([]byte(dataMap["header"].(string)), &headerMap)
+	feign.Header = headerMap
+	bodyMap := make(map[string]interface{})
+	json.Unmarshal([]byte(dataMap["body"].(string)), &bodyMap)
+	feign.Body = bodyMap
+
+	return feign
+}
+
+func (feign *Feign) Target() {
+	ConfigLib := new(lib.Config)
+	HttpRequestLib := new(lib.HttpRequest)
+
+	// 请求服务不存在
+	if !ConfigLib.Get("api_url." + feign.BaseUrl).IsExists() {
+	}
+
+	url := ConfigLib.Get("api_url."+feign.BaseUrl).String() + feign.ActionUrl
+	HttpRequestLib.Method = feign.Method
+	HttpRequestLib.Header = feign.Header
+	HttpRequestLib.Url = url
+	HttpRequestLib.Params = feign.Body
+	res, err := HttpRequestLib.FormRequest()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("===========================> " + HttpRequestLib.Method + " " + HttpRequestLib.Url)
+	fmt.Println(res)
 }
