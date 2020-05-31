7f3677a946004b9f99d5182f014d11226f04cf2d
kisschou <kisschou@me.com>
2020-06-01 01:43:52 +0800

    添加二进制post请求方法

--- tdog/lib/http_request.go
+++ tdog/lib/http_request.go
@@ -63,3 +63,66 @@ func (hp *HttpRequest) FormRequest() (resData string, err error) {
 	resData = string(body)
 	return
 }
+
+/*
+ * // 发送二进制数据流
+ * // header
+ * header["Authorization"] = resMap["authorization"].(string)
+ * header["Content-Type"] = "text/plain"
+ * header["Connection"] = "Keep-Alive"
+ * // params
+ * params["method"] = "POST"
+ * params["base_url"] = "user_url"
+ * params["action_url"] = "/member/login"
+ * params["header"] = ""
+ * params["body"] = ""
+ * sendHeader := make(map[string]string)
+ * sendHeader["Authorization"] = resMap["authorization"].(string)
+ * sendHeaderJson, _ := json.Marshal(sendHeader)
+ * params["header"] = sendHeaderJson
+ * sendBody := make(map[string]interface{})
+ * sendBody["username"] = "admin"
+ * sendBody["password"] = "$2a$10$fP.426qKaTmix50Oln8L.uav55gELhAd0Eg66Av4oG86u8km7D/Ky"
+ * sendBodyJson, _ := json.Marshal(sendBody)
+ * params["body"] = sendBodyJson
+ *
+ * HttpRequestLib.Method = "POST"
+ * HttpRequestLib.Header = header
+ * HttpRequestLib.Url = "http://127.0.0.1:8000/feign/http"
+ * HttpRequestLib.Params = params
+ * res, err = HttpRequestLib.BytesPost()
+ * if err != nil {
+ *	fmt.Println(err)
+ * }
+ * mt.Println("===========================> " + HttpRequestLib.Method + " " + HttpRequestLib.Url)
+ * fmt.Println(res)
+ */
+func (hp *HttpRequest) BytesPost() (string, error) {
+	data, _ := json.Marshal(hp.Params)
+	body := bytes.NewReader(data)
+	req, err := http.NewRequest(hp.Method, hp.Url, body)
+	if err != nil {
+		logger := Logger{Level: 0, Key: "error"}
+		logger.New(err.Error())
+		return "", err
+	}
+
+	for k, v := range hp.Header {
+		req.Header.Set(k, v)
+	}
+
+	var resp *http.Response
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		logger := Logger{Level: 0, Key: "error"}
+		logger.New(err.Error())
+		return "", err
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logger := Logger{Level: 0, Key: "error"}
+		logger.New(err.Error())
+	}
+	return string(b), err
+}
