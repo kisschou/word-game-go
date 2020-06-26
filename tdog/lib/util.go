447a274bdf6ce353a57e564902bda599b9c6fdd1
kisschou <kisschou@me.com>
2020-06-26 19:47:53 +0800

    添加用户名类型判断和获取设备id等基础方法

--- tdog/lib/util.go
+++ tdog/lib/util.go
@@ -2,7 +2,11 @@ package lib
 
 import (
 	"math/rand"
+	"net"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -65,4 +69,71 @@ func (u *Util) InMap(key string, dataMap map[string]interface{}) bool {
 		return true
 	}
 	return false
+}
+
+// 检测字符串是邮件、手机号、字符串
+// 返回: 0字符串1邮件2手机号
+func (u *Util) CheckStrType(str string) int {
+	if verifyEmailFormat(str) {
+		return 1
+	}
+
+	if verifyPhoneFormat(str) {
+		return 2
+	}
+
+	return 0
+}
+
+func verifyEmailFormat(email string) bool {
+	// pattern := `\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*`
+	pattern := `^[0-9a-z][_.0-9a-z-]{0,31}@([0-9a-z][0-9a-z-]{0,30}[0-9a-z]\.){1,4}[a-z]{2,4}$`
+	reg := regexp.MustCompile(pattern)
+	return reg.MatchString(email)
+}
+
+func verifyPhoneFormat(phone string) bool {
+	pattern := "^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\\d{8}$"
+	reg := regexp.MustCompile(pattern)
+	return reg.MatchString(phone)
+}
+
+// 获取设备id
+// 通过网卡ipv4生成
+func (u *Util) GetMachineId() int64 {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return 127000000001
+	}
+
+	gatewayIp := ""
+	for _, address := range addrs {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				gatewayIp = ipnet.IP.String()
+				break
+			}
+		}
+	}
+
+	gatewayIpSplit := strings.Split(gatewayIp, ".")
+	gatewayIp = ""
+	for _, v := range gatewayIpSplit {
+		if len(v) < 3 {
+			for i := len(v); i < 3; i++ {
+				v = "0" + v
+			}
+		}
+		gatewayIp += v
+	}
+	machineId, _ := strconv.ParseInt(gatewayIp, 10, 64)
+	return machineId
+}
+
+func (u *Util) RandInt64(min, max int64) int64 {
+	if min >= max || min == 0 || max == 0 {
+		return max
+	}
+	rand.Seed(time.Now().UnixNano())
+	return rand.Int63n(max-min) + min
 }
